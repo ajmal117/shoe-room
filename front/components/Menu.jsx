7c812bf24ea31522bc72f0fcd4a97d2232aa4623
ajmal ali <ajmal.mzn95@gmail.com>
2024-03-25 00:30:03 +0530

    first commit

--- front/components/Menu.jsx
+++ front/components/Menu.jsx
@@ -8,6 +8,7 @@ const data = [
   { id: 2, name: "About", url: "/about" },
   { id: 3, name: "Categories", subMenu: true },
   { id: 4, name: "Contact", url: "/contact" },
+  { id: 5, name: "Contact2", url: "/contact2" },
 ];
 
 const subMenuData = [
