bbb57f8603feea76686aba9d21a6a5690a71698a
ajmal ali <ajmal.mzn95@gmail.com>
2024-03-26 02:07:04 +0530

    product pages added

---
+++ front/pages/product/[slug].js
@@ -0,0 +1,88 @@
+import ProductDetailsCarousel from "@/components/ProductDetailsCarousel";
+import Wrapper from "@/components/Wrapper";
+import React from "react";
+import { IoMdHeartEmpty } from "react-icons/io";
+
+const productDetails = () => {
+  return (
+    <div className="w-full md:py-20">
+      <Wrapper>
+        <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
+          {/* left column start  */}
+          <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
+            <ProductDetailsCarousel />
+          </div>
+          {/* left column end */}
+
+          {/* right column start */}
+          <div className="flex-[1] py-3">
+            {/* product title */}
+            <div className="text-[34px] font-semibold mb-2">
+              Jordan Retro 6 G
+            </div>
+
+            {/* product subtitle  */}
+            <div className="text-lg font mb-5">Men&apos; s Golf Shoes</div>
+
+            {/* product price  */}
+            <div className="text-lg font-semibold">MRP : &#8377; 19 695.00</div>
+
+            {/* product tax */}
+            <div className="text-md font-medium text-black/[0.5]">
+              incl. of taxes
+            </div>
+
+            <div className="text-md font-medium text-black/[0.5] mb-20">
+              {`(Also includes all applicable duties)`}
+            </div>
+
+            {/* product size range start  */}
+            <div className="mb-10">
+              {/* heading start  */}
+              <div className="flex justify-between mb-2">
+                <div className="text-md font-semibold">Select Size</div>
+                <div className="text-md font-medium text-black/[0.5] cursor-pointer">
+                  Select Guide
+                </div>
+              </div>
+              {/* heading end  */}
+              {/* size start  */}
+              <div className=" grid grid-cols-3 gap-2">
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+              </div>
+              {/* size end  */}
+            </div>
+
+            {/* product size range end  */}
+          </div>
+          {/* right column end */}
+        </div>
+      </Wrapper>
+    </div>
+  );
+};
+
+export default productDetails;
