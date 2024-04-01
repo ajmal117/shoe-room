5c053852f1101382308858e5d5239d639dbef498
ajmal ali <ajmal.mzn95@gmail.com>
2024-04-02 04:25:13 +0530

    api added

--- front/pages/index.js
+++ front/pages/index.js
@@ -2,22 +2,12 @@ import HeroBanner from "@/components/HeroBanner";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 import { fetchDataFromApi } from "@/utils/api";
-import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    fetchProduct;
-  }, []);
-
-  const fetchProduct = async () => {
-    const { data } = fetchDataFromApi("/api/products");
-    setData(data);
-  };
-
+export default function Home({ products }) {
   return (
     <main className="">
       <HeroBanner />
+      {/* <h1>{products?.data?.[0]?.attributes?.name}</h1> */}
       <Wrapper>
         {/* heading and paragraph start */}
         <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
@@ -34,18 +24,31 @@ export default function Home() {
 
         {/* product grid  start*/}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          {products?.data?.map((product) => (
+            <ProductCard key={product?.id} data={product} />
+          ))}
+
+          {/* <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <ProductCard /> */}
         </div>
         {/* product grid end */}
       </Wrapper>
     </main>
   );
+}
+
+// use ssg and ssr outside after the fuction compononts
+
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+
+  return {
+    props: { products },
+  };
 }
